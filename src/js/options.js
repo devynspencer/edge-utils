1761fb186acf99d2bf8400bc16787be663a4ea8c
Devyn Spencer <devynspencer@users.noreply.github.com>
2024-07-22 22:44:48 -0700

    Add buttons to options popout

--- src/js/options.js
+++ src/js/options.js
@@ -1,8 +1,43 @@
+document.getElementById("validateConfig")
+    .addEventListener("click", handleValidateConfig);
+
 document.getElementById("applyConfig")
     .addEventListener("click", handleApplyConfig);
 
 document.getElementById("showConfig")
     .addEventListener("click", handleShowConfig);
+
+function handleValidateConfig() {
+    console.log("Validating configuration...");
+
+    let content = {};
+    const fileInput = document.getElementById("inputConfigFile");
+
+    if (fileInput.files.length > 0) {
+        const reader = new FileReader();
+
+        console.log(`Loading config from file '${fileInput.files[0].name}'...`);
+
+        reader.readAsText(fileInput.files[0]);
+        reader.onloadend = () => {
+            try {
+                content = JSON.parse(reader.result);
+
+                if (validateConfig(content)) {
+                    document.getElementById("message").innerText = "Configuration is valid!";
+                }
+
+                else {
+                    document.getElementById("message").innerText = "Configuration is invalid!";
+                }
+            }
+
+            catch (error) {
+                console.error("Error reading file: ", error);
+            }
+        };
+    }
+}
 
 function handleApplyConfig() {
     console.log("Applying configuration...");
