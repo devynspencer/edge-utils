c20695ca1e5ce583d23cf9364b5e4308422e43f7
Devyn Spencer <devynspencer@users.noreply.github.com>
2024-08-01 18:19:48 -0700

    Remove unused command

--- src/js/background.js
+++ src/js/background.js
@@ -2,11 +2,6 @@ importScripts('shared.js');
 
 // Handle commands from manifest.json
 chrome.commands.onCommand.addListener(function (command) {
-    // Open extension popup on shortcut
-    if (command === 'toggle-popup-display') {
-        chrome.tabs.create({ url: "src/popup.html" });
-    }
-
     // Pin current tab on shortcut
     if (command === 'toggle-pin-current-tab') {
         toggleActiveTabPin();
