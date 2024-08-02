941e4bc573b9667ab1851fc405def7d19ba527c7
Devyn Spencer <devynspencer@users.noreply.github.com>
2024-08-01 18:16:08 -0700

    Add function copyToClipboard

--- src/js/popup.js
+++ src/js/popup.js
@@ -4,6 +4,18 @@ function updateNotificationMessage(message) {
     messageElement.textContent = message;
 }
 
+function copyToClipboard(text) {
+    // TODO: Why not just copy directly to the clipboard
+    const textarea = document.createElement("textarea");
+
+    textarea.value = text;
+    textarea.style.position = "fixed";
+    textarea.style.opacity = "0";
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand("copy");
+    document.body.removeChild(textarea);
+}
 document.getElementById("toggleActiveTabPin")
     .addEventListener("click", () => {
         updateNotificationMessage("Pinning active tab ...");
