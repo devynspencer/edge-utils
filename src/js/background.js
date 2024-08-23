7c49af0ad1b99f1537bccb2e83dabbd2244e1bcc
Devyn Spencer <devynspencer@users.noreply.github.com>
2024-08-23 13:01:49 -0700

    Update TODO items

--- src/js/background.js
+++ src/js/background.js
@@ -1,5 +1,7 @@
 importScripts('shared.js');
 
+// TODO: Add a context menu to run scripts on a page
+
 // Handle commands from manifest.json
 chrome.commands.onCommand.addListener(function (command) {
     // Pin current tab on shortcut
@@ -11,6 +13,8 @@ chrome.commands.onCommand.addListener(function (command) {
         muteAllTabs();
     }
 
+    // TODO: Organize tabs in current window on shortcut
+    // TODO: Should ungrouped tabs be moved to a default group? -- *helping to differentiate newly opened tabs from existing tabs*
     // Organize tabs in current window on shortcut
     if (command === 'organize-tabs') {
         organizeTabs();
